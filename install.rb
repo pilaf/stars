75c0755e00fe3adda4a20341be050c21ea16a7a7
Pilaf <pfayolle@gmail.com>
2010-02-02 16:05:31 -0300

    Added initializer to install and uninstall scripts.

--- install.rb
+++ install.rb
@@ -1,4 +1,6 @@
-# Copy assets to main application
 require Rails.root.join('config/environment')
 require 'rails_generator'
+
+# Copy assets and config to application
 Rails::Generator::Base.instance(:stars_rating_assets).command(:create).invoke!
+Rails::Generator::Base.instance(:stars_rating_config).command(:create).invoke!
--- uninstall.rb
+++ uninstall.rb
@@ -1,4 +1,6 @@
-# Remove assets from main application
 require Rails.root.join('config/environment')
 require 'rails_generator'
+
+# Remove assets and config from application
 Rails::Generator::Base.instance(:stars_rating_assets).command(:destroy).invoke!
+Rails::Generator::Base.instance(:stars_rating_config).command(:destroy).invoke!
