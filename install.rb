32571161f9e75e62d57d2149479003a2889a0c0b
Pilaf <pfayolle@gmail.com>
2010-02-02 15:53:34 -0300

    Fixed install and uninstall scripts.

--- install.rb
+++ install.rb
@@ -1,3 +1,4 @@
 # Copy assets to main application
+require Rails.root.join('config/environment')
 require 'rails_generator'
 Rails::Generator::Base.instance(:stars_rating_assets).command(:create).invoke!
--- uninstall.rb
+++ uninstall.rb
@@ -1,3 +1,4 @@
 # Remove assets from main application
+require Rails.root.join('config/environment')
 require 'rails_generator'
-Rails::Generator::Base.instance(:stars_rating_assets).command(:create).invoke!
+Rails::Generator::Base.instance(:stars_rating_assets).command(:destroy).invoke!
